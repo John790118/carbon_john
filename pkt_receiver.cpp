3bcd6b8432af21fc523e1b724c1f3b5d62f665bb
John790118 <goodcross@outlook.com>
2021-10-19 21:37:49 +0800

    .\pkt_receiver.cpp

--- pkt_receiver.cpp
+++ pkt_receiver.cpp
@@ -22,7 +22,7 @@ void pkt_receiver :: pkt_receiver_prc()
 //        {
 //            pkt_receiver_file.open("pkt_receiver_file.log",ios::out | ios::app);
 //        }
-        for（int i=0; i < NO_PORTS; i++)
+        for(int i=0; i < NO_PORTS; i++)
         {
             temp_val[i] = pkt_in[i].read();
             cout << "@" << in_clk_cnt << "_clks stim receive =>:"
